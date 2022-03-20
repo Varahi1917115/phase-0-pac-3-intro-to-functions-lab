1b46c13b06848a5d293291ac62bd19a0a0ed7ca0
Samarth <Varahi1917115@gmail.com>
2022-03-19 22:06:55 -0400

    commit tree 2e4ef76053144764e56bcba9769ce2de1adc2cba

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string) {
+    console.log(string === string.toLowerCase());
+    if (string === string.toLowerCase()) {
+        return "I can't hear you!"
+
+function sayHiToHeadphonedRoommate(string) {
+    console.log(string === string.toUpperCase());
+    if (string === string.toUpperCase()) {
+        return "YES INDEED"
+    }
+}
+
+    }
+}
